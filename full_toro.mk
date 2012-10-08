9b9952b9aae46872d02120b27becf43f67e44401
Greg_Maahs <projecteliteroms@gmail.com>
2012-10-07 20:06:56 -0400

    Build nexus parts from here

--- full_toro.mk
+++ full_toro.mk
@@ -21,8 +21,9 @@
 # lines, full and toro, hence its name.
 #
 
-# Camera and Gallery
+# 
 PRODUCT_PACKAGES := \
+    GNexusParts
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
